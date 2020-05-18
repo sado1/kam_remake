3d287764e9ecdbb36bd1189024f065b02088334a
Lewin <Hodgman>
2020-05-18 14:39:06 +1000

    Refactor, initialise Prev and Curr

--- src/units/KM_UnitVisual.pas
+++ src/units/KM_UnitVisual.pas
@@ -11,6 +11,7 @@ type
     SlideX, SlideY: Single;
     Action: TKMUnitActionType;
     AnimStep: Integer;
+    procedure SetFromUnit(aUnit: TObject);
   end;
 
   // Purely visual thing. Split from TKMUnit to aviod mixup of game-logic and render Positions
@@ -32,31 +33,39 @@ uses
   KM_Units, KM_Game, KM_Terrain;
 
 
+{ TKMUnitVisualState }
+procedure TKMUnitVisualState.SetFromUnit(aUnit: TObject);
+var
+  U: TKMUnit;
+begin
+  U := TKMUnit(aUnit);
+  PosF := U.PositionF;
+  Dir := U.Direction;
+  SlideX := U.GetSlide(axX);
+  SlideY := U.GetSlide(axY);
+  AnimStep := U.AnimStep;
+
+  if U.Action <> nil then
+    Action := U.Action.ActionType
+  else
+    Action := uaUnknown;
+end;
+
+
 { TKMUnitVisual }
 constructor TKMUnitVisual.Create(aUnit: TObject);
 begin
   inherited Create;
   fUnit := TKMUnit(aUnit);
+  Prev.SetFromUnit(fUnit);
+  Curr.SetFromUnit(fUnit);
 end;
 
 
 procedure TKMUnitVisual.UpdateState;
-var
-  U: TKMUnit;
 begin
-  U := TKMUnit(fUnit);
   Prev := Curr;
-  Curr.PosF := U.PositionF;
-  Curr.Dir := U.Direction;
-  Curr.SlideX := U.GetSlide(axX);
-  Curr.SlideY := U.GetSlide(axY);
-  Curr.AnimStep := U.AnimStep;
-
-  if U.Action <> nil then
-    Curr.Action := U.Action.ActionType
-  else
-    Curr.Action := uaUnknown;
+  Curr.SetFromUnit(fUnit);
 end;
-
 
 end.
