e8ed4fbb3983a14c0d972f291412b7d120de21df
Rey <levchenko.and@gmail.com>
2021-05-07 20:20:44 +0300

    split update_revision.bat into 2 bat-files

--- bat/update_revision.bat
+++ bat/update_revision.bat
@@ -1,16 +1,6 @@
 
-@REM get revision number from git
-@FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list --count HEAD`) DO (
-  @SET kam_revision=%%F
-)
-@REM increment revision number by 1, as we are going to make one more commit for KM_Revision.inc
-@SET /A kam_revision=kam_revision+1
-
-@REM update revision number in KM_Revision.inc file 
-@echo GAME_REVISION_NUM=%kam_revision% > ..\KM_Revision.inc
-
-@REM update revision for Installer
-@echo #define Revision 'r%kam_revision%' > ..\Installer\Revision.iss
+@REM
+call update_revision_locally.bat
 
 @REM add, commit and push changes to github
 git reset
