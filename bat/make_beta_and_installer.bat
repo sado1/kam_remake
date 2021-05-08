ff4bc8e36b1bcaf93a31e22d4009c9a6831fec06
Rey <levchenko.and@gmail.com>
2021-05-08 14:30:58 +0300

    use timecmd.bat to measure times in the make_beta_and_installer.bat

--- bat/make_beta_and_installer.bat
+++ bat/make_beta_and_installer.bat
@@ -1,22 +1,31 @@
 @echo off
 
-@REM using timer util made by https://www.gammadyne.com/cmdline.htm#timer
-if "%~1"=="-timer" (
-  call timer.exe /nologo 
-)
+@REM we could use timer util made by https://www.gammadyne.com/cmdline.htm#timer
+@REM if "%~1"=="-timer" (
+@REM   call timer.exe /nologo 
+@REM )
 
-call create_beta.bat
+@REM or we could measure execution time in seconds via next calls
+@REM @set /A _tic=%time:~0,2%*3600^+%time:~3,1%*10*60^+%time:~4,1%*60^+%time:~6,1%*10^+%time:~7,1% >nul
+
+@REM let's use timecmd.bat to measure execution time 
+call timecmd.bat create_beta.bat
 
 if errorlevel 2 (goto exit2)
 
-if "%~1"=="-timer" (
-  @echo #
-  echo Create Beta took:
-  call "timer.exe" /nologo /r
-  
-  @echo Create Beta took: > build_time.txt
-  @call "timer.exe" /nologo /r >> build_time.txt
-)
+@REM @set /A _toc=%time:~0,2%*3600^+%time:~3,1%*10*60^+%time:~4,1%*60^+%time:~6,1%*10^+%time:~7,1% >nul
+@REM @set /A _elapsed=%_toc%-%_tic
+@REM @echo %_elapsed% seconds.
+
+
+@REM if "%~1"=="-timer" (
+@REM   @echo #
+@REM   echo Create Beta took:
+@REM   call "timer.exe" /nologo /r
+@REM   
+@REM   @echo Create Beta took: > build_time.txt
+@REM   @call "timer.exe" /nologo /r >> build_time.txt
+@REM )
 
 call prepare_for_installer.bat
 
@@ -27,18 +36,18 @@ call prepare_for_installer.bat
 @echo #  
 @echo #
 
-call "%ISC_DIR%"\iscc.exe /Q "..\Installer\InstallerFull.iss" 
+call timecmd.bat "%ISC_DIR%"\iscc.exe /Q "..\Installer\InstallerFull.iss" 
 
-if "%~1"=="-timer" (
-  echo Inno Setup compilation took:
-  call "timer.exe" /nologo /S
-  
-  @echo #
-  @echo --------------------------------------------------------- >> build_time.txt 
-  @echo #
-  @echo Inno Setup compilation took: >> build_time.txt
-  @call "timer.exe" /nologo /r >> build_time.txt
-)
+@REM if "%~1"=="-timer" (
+@REM   echo Inno Setup compilation took:
+@REM   call "timer.exe" /nologo /S
+@REM   
+@REM   @echo #
+@REM   @echo --------------------------------------------------------- >> build_time.txt 
+@REM   @echo #
+@REM   @echo Inno Setup compilation took: >> build_time.txt
+@REM   @call "timer.exe" /nologo /r >> build_time.txt
+@REM )
 
 @echo #
 @echo #----------------------------------------------------------#
