e79036f8a567ed527da557c71b2131de6d2c8e5d
Rey <levchenko.and@gmail.com>
2021-05-08 10:08:29 +0300

    Add timer util to measure build / compilation time

--- bat/make_beta_and_installer.bat
+++ bat/make_beta_and_installer.bat
@@ -1,7 +1,8 @@
 @echo off
 
+@REM using timer util made by https://www.gammadyne.com/cmdline.htm#timer
 if "%~1"=="-timer" (
-  call timer.exe /nologo /n /r
+  call timer.exe /nologo 
 )
 
 call create_beta.bat
@@ -9,27 +10,37 @@ call create_beta.bat
 if errorlevel 2 (goto exit2)
 
 if "%~1"=="-timer" (
-  call "timer.exe" /nologo /n /r
-  @REM call "timer.exe"
+  @echo #
+  echo Create Beta took:
+  call "timer.exe" /nologo /r
+  
+  @echo Create Beta took: > build_time.txt
+  @call "timer.exe" /nologo /r >> build_time.txt
 )
 
 call prepare_for_installer.bat
-
 
 @echo #                                                          # 
 @echo #==========================================================#
 @echo #             Start Inno Setup compilation                 #  
 @echo #==========================================================# 
 @echo #  
-@echo .                                                       
+@echo #
 
-call "%ISS_DIR%"\iscc.exe /Q "..\Installer\InstallerFull.iss" 
+call "%ISC_DIR%"\iscc.exe /Q "..\Installer\InstallerFull.iss" 
 
 if "%~1"=="-timer" (
+  echo Inno Setup compilation took:
   call "timer.exe" /nologo /S
+  
+  @echo #
+  @echo --------------------------------------------------------- >> build_time.txt 
+  @echo #
+  @echo Inno Setup compilation took: >> build_time.txt
+  @call "timer.exe" /nologo /r >> build_time.txt
 )
 
-@echo .
+@echo #
 @echo #----------------------------------------------------------#
 @echo #                Compilation completed !                   #  
 @echo #----------------------------------------------------------#
