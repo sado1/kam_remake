f822a8c60a54001009c710c61438de815b3af9f8
Rey <levchenko.and@gmail.com>
2021-05-18 19:16:16 +0300

    MapEd: add detailed goal description

--- src/gui/pages_maped/player/KM_GUIMapEdPlayerGoalPopUp.pas
+++ src/gui/pages_maped/player/KM_GUIMapEdPlayerGoalPopUp.pas
@@ -19,12 +19,13 @@ type
     function GetVisible: Boolean;
   protected
     Panel_Goal: TKMPanel;
-    Image_GoalFlag: TKMImage;
-    Radio_GoalType: TKMRadioGroup;
-    Radio_GoalCondition: TKMRadioGroup;
-    NumEdit_GoalPlayer: TKMNumericEdit;
-    Button_GoalOk: TKMButton;
-    Button_GoalCancel: TKMButton;
+      Image_GoalFlag: TKMImage;
+      Radio_GoalType: TKMRadioGroup;
+      Radio_GoalCondition: TKMRadioGroup;
+      NumEdit_GoalPlayer: TKMNumericEdit;
+      Label_GoalDescription: TKMLabel;
+      Button_GoalOk: TKMButton;
+      Button_GoalCancel: TKMButton;
   public
     fOnDone: TNotifyEvent;
     constructor Create(aParent: TKMPanel);
@@ -37,16 +38,13 @@ type
 
 implementation
 uses
-  KM_HandsCollection, KM_ResTexts, KM_RenderUI, KM_ResFonts, KM_Hand, KM_MapTypes;
-
+  KM_HandsCollection, KM_ResTexts, KM_RenderUI, KM_ResFonts, KM_Hand, KM_MapUtils, KM_MapTypes;
 
 { TKMGUIMapEdGoal }
 constructor TKMMapEdPlayerGoal.Create(aParent: TKMPanel);
 const
-  SIZE_X = 600;
+  SIZE_X = 700;
   SIZE_Y = 300;
-var
-  img: TKMImage;
 begin
   inherited Create;
 
@@ -55,36 +53,39 @@ begin
   Panel_Goal.Hide;
 
   TKMBevel.Create(Panel_Goal, -2000,  -2000, 5000, 5000);
-  img := TKMImage.Create(Panel_Goal, -20, -50, SIZE_X+40, SIZE_Y+60, 15, rxGuiMain);
-  img.ImageStretch;
+  with TKMImage.Create(Panel_Goal, -20, -50, SIZE_X+40, SIZE_Y+60, 15, rxGuiMain) do
+    ImageStretch;
   TKMBevel.Create(Panel_Goal,   0,  0, SIZE_X, SIZE_Y);
   TKMLabel.Create(Panel_Goal, SIZE_X div 2, 10, gResTexts[TX_MAPED_GOALS_TITLE], fntOutline, taCenter);
 
   Image_GoalFlag := TKMImage.Create(Panel_Goal, 10, 10, 0, 0, 30, rxGuiMain);
 
   TKMLabel.Create(Panel_Goal, 20, 40, 160, 0, gResTexts[TX_MAPED_GOALS_TYPE], fntMetal, taLeft);
-  Radio_GoalType := TKMRadioGroup.Create(Panel_Goal, 20, 60, 160, 60, fntMetal);
-  Radio_GoalType.Add(gResTexts[TX_MAPED_GOALS_TYPE_NONE], False);
+  Radio_GoalType := TKMRadioGroup.Create(Panel_Goal, 20, 60, 160, 40, fntMetal);
+  Radio_GoalType.Add(gResTexts[TX_MAPED_GOALS_TYPE_NONE], False, False);
   Radio_GoalType.Add(gResTexts[TX_MAPED_GOALS_TYPE_VICTORY]);
   Radio_GoalType.Add(gResTexts[TX_MAPED_GOALS_TYPE_SURVIVE]);
   Radio_GoalType.OnChange := Goal_Change;
 
   TKMLabel.Create(Panel_Goal, 200, 40, 280, 0, gResTexts[TX_MAPED_GOALS_CONDITION], fntMetal, taLeft);
-  Radio_GoalCondition := TKMRadioGroup.Create(Panel_Goal, 200, 60, 280, 180, fntMetal);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_NONE], False);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_TUTORIAL], False);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_TIME], False);
+  Radio_GoalCondition := TKMRadioGroup.Create(Panel_Goal, 200, 60, 280, 100, fntMetal);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_NONE], False, False);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_TUTORIAL], False, False);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_TIME], False, False);
   Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_BUILDS]);
   Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_TROOPS]);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_UNKNOWN], False);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_ASSETS]);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_SERFS]);
-  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_ECONOMY]);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_UNKNOWN], False, False);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_ASSETS], False);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_SERFS], False);
+  Radio_GoalCondition.Add(gResTexts[TX_MAPED_GOALS_CONDITION_ECONOMY], False);
   Radio_GoalCondition.OnChange := Goal_Change;
 
   TKMLabel.Create(Panel_Goal, 480, 40, gResTexts[TX_MAPED_GOALS_PLAYER], fntMetal, taLeft);
   NumEdit_GoalPlayer := TKMNumericEdit.Create(Panel_Goal, 480, 60, 1, MAX_HANDS);
   NumEdit_GoalPlayer.OnChange := Goal_Change;
+
+  Label_GoalDescription := TKMLabel.Create(Panel_Goal, 20, 180, SIZE_X - 40, 20, '', fntMetal, taLeft);
+  Label_GoalDescription.AutoWrap := True;
 
   Button_GoalOk := TKMButton.Create(Panel_Goal, SIZE_X-20-320-10, SIZE_Y - 50, 160, 30, gResTexts[TX_MAPED_OK], bsMenu);
   Button_GoalOk.OnClick := Goal_Close;
@@ -94,10 +95,22 @@ end;
 
 
 procedure TKMMapEdPlayerGoal.Goal_Change(Sender: TObject);
+var
+  gc: TKMGoalCondition;
 begin
+  gc := TKMGoalCondition(Radio_GoalCondition.ItemIndex);
   //Settings get saved on close, now we just toggle fields
   //because certain combinations can't coexist
-  NumEdit_GoalPlayer.Enabled := TKMGoalCondition(Radio_GoalCondition.ItemIndex) <> gcTime;
+  NumEdit_GoalPlayer.Enabled := gc <> gcTime;
+
+  if NumEdit_GoalPlayer.Enabled then
+    Label_GoalDescription.Caption := GetGoalDescription(gMySpectator.HandID, NumEdit_GoalPlayer.Value - 1,
+                                                        TKMGoalType(Radio_GoalType.ItemIndex), gc,
+                                                        gMySpectator.Hand.FlagTextColor,
+                                                        gHands[NumEdit_GoalPlayer.Value - 1].FlagTextColor,
+                                                        icRoyalYellow, icLightOrange)
+  else
+    Label_GoalDescription.Caption := '';
 end;
 
 
--- src/mission/KM_MapUtils.pas
+++ src/mission/KM_MapUtils.pas
@@ -2,7 +2,7 @@ unit KM_MapUtils;
 {$I KaM_Remake.inc}
 interface
 uses
-  KM_MapTypes;
+  KM_Defaults, KM_MapTypes;
 
   function GuessMPPathRel(const aName, aExt: string; aCRC: Cardinal): string;
   function GuessMissionPathRel(const aMissionFileRelSP, aMissionName: string; aMapFullCRC: Cardinal; aIsMultiplayer: Boolean): string;
@@ -10,10 +10,13 @@ uses
   function DetermineMapFolder(const aFolderName: UnicodeString; out aMapFolder: TKMapFolder): Boolean;
   function GetMapFolderType(aIsMultiplayer: Boolean): TKMapFolder;
 
+  function GetGoalDescription(aPlayer1, aPlayer2: TKMHandID; aGoalType: TKMGoalType; aGoalCondition: TKMGoalCondition;
+                              aColPlayer1, aColPlayer2, aColTxt, aColBld: Cardinal): string;
+
 implementation
 uses
   SysUtils,
-  KM_Defaults;
+  KM_Resource, KM_ResTexts, KM_ResHouses, KM_ResTypes, KM_CommonUtils;
 
 function GuessMPPathRel(const aName, aExt: string; aCRC: Cardinal): string;
 var
@@ -60,6 +63,64 @@ begin
   else
     Result := mfSP;
 end;
+
+
+// Format mission goal description
+function GetGoalDescription(aPlayer1, aPlayer2: TKMHandID; aGoalType: TKMGoalType; aGoalCondition: TKMGoalCondition;
+                            aColPlayer1, aColPlayer2, aColTxt, aColBld: Cardinal): string;
+type
+  TKMGoalTypeDescWordKind = (dwkFirst, dwkSecond);
+const
+  GOAL_TYPE_DESC_TX: array[TKMGoalType, TKMGoalTypeDescWordKind] of Integer = (
+    (TX_MAPED_GOALS_TYPE_NONE,            TX_MAPED_GOALS_TYPE_NONE),
+    (TX_GOAL_CONDITION_DESC_WIN_WORD,     TX_GOAL_CONDITION_DESC_DESTROYED_WORD),
+    (TX_GOAL_CONDITION_DESC_SURVIVE_WORD, TX_GOAL_CONDITION_DESC_SAVED_WORD)
+  );
+
+  GOAL_COND_DESC_WORD_TX: array[TKMGoalCondition] of Integer = (
+    TX_MAPED_GOALS_CONDITION_NONE,
+    TX_MAPED_GOALS_CONDITION_TUTORIAL,
+    TX_MAPED_GOALS_CONDITION_TIME,
+    TX_MAPED_GOALS_CONDITION_BUILDS,
+    TX_MAPED_GOALS_CONDITION_TROOPS,
+    TX_MAPED_GOALS_CONDITION_UNKNOWN,
+    TX_MAPED_GOALS_CONDITION_ASSETS,
+    TX_MAPED_GOALS_CONDITION_SERFS,
+    TX_MAPED_GOALS_CONDITION_ECONOMY
+  );
+
+var
+  I: Integer;
+  goalCondStr, housesStr: string;
+begin
+  case aGoalCondition of
+    gcBuildings:
+      begin
+        housesStr := '';
+        for I := Low(GOAL_BUILDINGS_HOUSES) to High(GOAL_BUILDINGS_HOUSES) do
+        begin
+          if housesStr <> '' then
+            housesStr := housesStr + ', ';
+
+          housesStr := housesStr + gRes.Houses[GOAL_BUILDINGS_HOUSES[I]].HouseName;
+        end;
+        goalCondStr := gResTexts[TX_GOAL_CONDITION_COND_DESC_BUILDINGS, [WrapWrappedColor(housesStr, aColBld)]];
+      end;
+    else
+      goalCondStr := gResTexts[GOAL_COND_DESC_WORD_TX[aGoalCondition]];
+  end;
+
+  // Format text out of string like:
+  // 'For player %0:s to %1:s player's %2:s %3:s must be %4:s'
+  // We want to get smth like (depends of the locale, ofc)
+  // 'For player 1 to win player's 2 troops must be destroyed'
+  Result := gResTexts[TX_GOAL_CONDITION_DESC_PATTERN, [WrapColor(aPlayer1 + 1, aColPlayer1),
+                                                       WrapColor(gResTexts[GOAL_TYPE_DESC_TX[aGoalType, dwkFirst]], aColTxt),
+                                                       WrapColor(aPlayer2 + 1, aColPlayer2),
+                                                       WrapColor(goalCondStr, aColTxt),
+                                                       WrapColor(gResTexts[GOAL_TYPE_DESC_TX[aGoalType, dwkSecond]], aColTxt)]];
+end;
+
 
 
 end.
