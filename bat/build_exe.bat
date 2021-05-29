340bc6e17ca1c1015136dec4fb8c3cd475cb4b68
Rey <levchenko.and@gmail.com>
2021-05-29 07:53:11 +0300

    fix build_exe.bat msbuild call

--- bat/build_exe.bat
+++ bat/build_exe.bat
@@ -9,7 +9,7 @@ echo called build_exe.bat
 call rsvars.bat
 
 REM build
-msbuild ..\KaM_Remake.dproj /p:Configuration=Release /t:Build /clp:ErrorsOnly /fl /flp:LogFile="build_exe.log"
+msbuild ..\KaM_Remake.dproj /p:config=Release /target:Build /p:platform=Win32 /clp:ErrorsOnly /fl /flp:LogFile="build_exe.log"
 
 REM build
 REM msbuild utils\ScriptValidator\ScriptValidator.dproj /p:Configuration=Debug /t:Build /clp:ErrorsOnly /fl /flp:LogFile="build_ScriptValidator_exe.log"
