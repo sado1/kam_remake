677e3c3056133287c256759c9c432fb6d3bd82ae
Rey <levchenko.and@gmail.com>
2022-05-08 19:10:50 +0300

    Do not allow zoom or scroll hotkeys while in cinematics

--- src/gui/KM_InterfaceGame.pas
+++ src/gui/KM_InterfaceGame.pas
@@ -367,6 +367,13 @@ var
 {$ENDIF}
 begin
   aHandled := True;
+
+  if gMySpectator.Hand.InCinematic then
+  begin
+    aHandled := False;
+    Exit;
+  end;
+
   //Scrolling
   if Key = gResKeys[kfScrollLeft]       then
     fViewport.ScrollKeyLeft  := True
@@ -427,6 +434,13 @@ end;
 procedure TKMUserInterfaceGame.HandleScrollKeysUp(Key: Word; var aHandled: Boolean);
 begin
   aHandled := True;
+
+  if gMySpectator.Hand.InCinematic then
+  begin
+    aHandled := False;
+    Exit;
+  end;
+
   //Scrolling
   if Key = gResKeys[kfScrollLeft]       then
     fViewport.ScrollKeyLeft := False
