4d8df025f629104eaff12859bb50c55b14bf0916
kromster80 <kromster80@gmail.com>
2023-01-21 22:32:36 +0600

    Almost perfect case to use the guard statement - one less indent level for the whole proc

--- Utils/RXXPacker/RXXPackerProc.pas
+++ Utils/RXXPacker/RXXPackerProc.pas
@@ -75,108 +75,107 @@ var
   dir: TKMDirection;
 begin
   //ruCustom sprite packs do not have a main RXX file so don't need packing
-  if RXInfo[RT].Usage <> ruCustom then
-  begin
-    rxName := fSpritesSourcePath + SPRITES_RES_DIR + '\' + RXInfo[RT].FileName + '.rx';
+  if RXInfo[RT].Usage = ruCustom then Exit;
 
-    if (RT <> rxTiles) and not FileExists(rxName) then
-      raise Exception.Create('Cannot find ' + rxName + ' file.' + sLineBreak + 'Please copy the file from your KaM\data\gfx\res\ folder.');
+  rxName := fSpritesSourcePath + SPRITES_RES_DIR + '\' + RXInfo[RT].FileName + '.rx';
 
-    spritePack := TKMSpritePackEdit.Create(RT, fPalettes);
-    try
-      //Load
-      if RT <> rxTiles then
-      begin
-        spritePack.LoadFromRXFile(rxName);
-        spritePack.OverloadRXDataFromFolder(fSpritesSourcePath + SPRITES_RES_DIR + '\', False); // Do not soften shadows, it will be done later on
-      end
-      else
-      if DirectoryExists(fSpritesSourcePath + SPRITES_RES_DIR + '\') then
-        spritePack.OverloadRXDataFromFolder(fSpritesSourcePath + SPRITES_RES_DIR + '\');
+  if (RT <> rxTiles) and not FileExists(rxName) then
+    raise Exception.Create('Cannot find ' + rxName + ' file.' + sLineBreak + 'Please copy the file from your KaM\data\gfx\res\ folder.');
 
-      //Tiles must stay the same size as they can't use pivots
-      if (RT <> rxTiles) and (gLog <> nil) then
-        gLog.AddTime('Trimmed ' + IntToStr(spritePack.TrimSprites));
+  spritePack := TKMSpritePackEdit.Create(RT, fPalettes);
+  try
+    //Load
+    if RT <> rxTiles then
+    begin
+      spritePack.LoadFromRXFile(rxName);
+      spritePack.OverloadRXDataFromFolder(fSpritesSourcePath + SPRITES_RES_DIR + '\', False); // Do not soften shadows, it will be done later on
+    end
+    else
+    if DirectoryExists(fSpritesSourcePath + SPRITES_RES_DIR + '\') then
+      spritePack.OverloadRXDataFromFolder(fSpritesSourcePath + SPRITES_RES_DIR + '\');
 
-      //Houses need some special treatment to adapt to GL_ALPHA_TEST that we use for construction steps
+    //Tiles must stay the same size as they can't use pivots
+    if (RT <> rxTiles) and (gLog <> nil) then
+      gLog.AddTime('Trimmed ' + IntToStr(spritePack.TrimSprites));
+
+    //Houses need some special treatment to adapt to GL_ALPHA_TEST that we use for construction steps
+    if RT = rxHouses then
+    begin
+      resHouses := TKMResHouses.Create;
+      spritePack.AdjoinHouseMasks(resHouses);
+      spritePack.GrowHouseMasks(resHouses);
+      spritePack.RemoveSnowHouseShadows(resHouses);
+      spritePack.RemoveMarketWaresShadows(resHouses);
+      resHouses.Free;
+    end;
+
+    // Determine objects size only for units (used for hitbox)
+    //todo: do we need it for houses too ?
+    if RT = rxUnits then
+      spritePack.DetermineImagesObjectSize;
+
+    //The idea was to blur the water and make it semitrasparent, but it did not worked out as expected
+    //if RT = rxTiles then
+    //  SpritePack.SoftWater(nil);
+
+    //Save
+    if PackToRXX then
+      spritePack.SaveToRXXFile(fRXXSavePath + 'data\Sprites\' + RXInfo[RT].FileName + '.rxx', AddVersionHeader);
+
+    //Generate alpha shadows for the following sprite packs
+    if RT in [rxHouses,rxUnits,rxGui,rxTrees] then
+    begin
       if RT = rxHouses then
       begin
-        resHouses := TKMResHouses.Create;
-        spritePack.AdjoinHouseMasks(resHouses);
-        spritePack.GrowHouseMasks(resHouses);
-        spritePack.RemoveSnowHouseShadows(resHouses);
-        spritePack.RemoveMarketWaresShadows(resHouses);
-        resHouses.Free;
+        spritePack.SoftenShadows(889, 892, False); //Smooth smoke
+        spritePack.SoftenShadows(1615, 1638, False); //Smooth flame
       end;
-
-      // Determine objects size only for units (used for hitbox)
-      //todo: do we need it for houses too ?
       if RT = rxUnits then
-        spritePack.DetermineImagesObjectSize;
-
-      //The idea was to blur the water and make it semitrasparent, but it did not worked out as expected
-      //if RT = rxTiles then
-      //  SpritePack.SoftWater(nil);
-
-      //Save
-      if PackToRXX then
-        spritePack.SaveToRXXFile(fRXXSavePath + 'data\Sprites\' + RXInfo[RT].FileName + '.rxx', AddVersionHeader);
-
-      //Generate alpha shadows for the following sprite packs
-      if RT in [rxHouses,rxUnits,rxGui,rxTrees] then
       begin
-        if RT = rxHouses then
-        begin
-          spritePack.SoftenShadows(889, 892, False); //Smooth smoke
-          spritePack.SoftenShadows(1615, 1638, False); //Smooth flame
-        end;
-        if RT = rxUnits then
-        begin
-          spritePack.SoftenShadows(6251, 6322, False); //Smooth thought bubbles
-          //Smooth all death animations for all units
-          resUnits := TKMResUnits.Create;
-          deathAnimCount := 0; //We need to remember which ones we've done because units reuse them
-          SetLength(deathAnimProcessed, 1000); //Hopefully more than enough
-          for UT := HUMANS_MIN to HUMANS_MAX do
-            for dir := dirN to dirNW do
-              for step := 1 to 30 do
+        spritePack.SoftenShadows(6251, 6322, False); //Smooth thought bubbles
+        //Smooth all death animations for all units
+        resUnits := TKMResUnits.Create;
+        deathAnimCount := 0; //We need to remember which ones we've done because units reuse them
+        SetLength(deathAnimProcessed, 1000); //Hopefully more than enough
+        for UT := HUMANS_MIN to HUMANS_MAX do
+          for dir := dirN to dirNW do
+            for step := 1 to 30 do
+            begin
+              spriteID := resUnits[UT].UnitAnim[uaDie,dir].Step[step]+1; //Sprites in units.dat are 0 indexed
+              if (spriteID > 0)
+              and not DeathAnimAlreadyDone(spriteID) then
               begin
-                spriteID := resUnits[UT].UnitAnim[uaDie,dir].Step[step]+1; //Sprites in units.dat are 0 indexed
-                if (spriteID > 0)
-                and not DeathAnimAlreadyDone(spriteID) then
-                begin
-                  spritePack.SoftenShadows(spriteID, False);
-                  deathAnimProcessed[deathAnimCount] := spriteID;
-                  Inc(deathAnimCount);
-                end;
+                spritePack.SoftenShadows(spriteID, False);
+                deathAnimProcessed[deathAnimCount] := spriteID;
+                Inc(deathAnimCount);
               end;
-          resUnits.Free;
-        end;
-
-        if RT = rxGui then
-        begin
-          spritePack.SoftenShadows(105, 128); //Field plans
-          spritePack.SoftenShadows(249, 281); //House tablets only (shadow softening messes up other rxGui sprites)
-          spritePack.SoftenShadows(461, 468); //Field fences
-          spritePack.SoftenShadows(660, 660); //Woodcutter cutting point sign
-        end
-        else
-          spritePack.SoftenShadows;
-
-        if PackToRXX then
-          spritePack.SaveToRXXFile(fRXXSavePath + 'data\Sprites\' + RXInfo[RT].FileName + '_a.rxx', AddVersionHeader);
-
-        if PackToRXA then
-        begin
-          if DirectoryExists(fSpritesSourcePath + SPRITES_INTERP_DIR + '\' + IntToStr(Ord(RT)+1) + '\') then
-            spritePack.OverloadRXDataFromFolder(fSpritesSourcePath + SPRITES_INTERP_DIR + '\' + IntToStr(Ord(RT)+1) + '\', False); // Shadows are already softened for interps
-
-          spritePack.SaveToRXAFile(fRXXSavePath + 'data\Sprites\' + RXInfo[RT].FileName + '.rxa', AddVersionHeader);
-        end;
+            end;
+        resUnits.Free;
       end;
-    finally
-      spritePack.Free;
+
+      if RT = rxGui then
+      begin
+        spritePack.SoftenShadows(105, 128); //Field plans
+        spritePack.SoftenShadows(249, 281); //House tablets only (shadow softening messes up other rxGui sprites)
+        spritePack.SoftenShadows(461, 468); //Field fences
+        spritePack.SoftenShadows(660, 660); //Woodcutter cutting point sign
+      end
+      else
+        spritePack.SoftenShadows;
+
+      if PackToRXX then
+        spritePack.SaveToRXXFile(fRXXSavePath + 'data\Sprites\' + RXInfo[RT].FileName + '_a.rxx', AddVersionHeader);
+
+      if PackToRXA then
+      begin
+        if DirectoryExists(fSpritesSourcePath + SPRITES_INTERP_DIR + '\' + IntToStr(Ord(RT)+1) + '\') then
+          spritePack.OverloadRXDataFromFolder(fSpritesSourcePath + SPRITES_INTERP_DIR + '\' + IntToStr(Ord(RT)+1) + '\', False); // Shadows are already softened for interps
+
+        spritePack.SaveToRXAFile(fRXXSavePath + 'data\Sprites\' + RXInfo[RT].FileName + '.rxa', AddVersionHeader);
+      end;
     end;
+  finally
+    spritePack.Free;
   end;
 end;
 
