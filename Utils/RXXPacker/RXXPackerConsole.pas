78f84301e0c5982d51788caf04cd9e43a9564a72
kromster80 <kromster80@gmail.com>
2023-01-25 20:36:19 +0600

    Refactoring into smaller chunks

--- Utils/RXXPacker/RXXPackerConsole.pas
+++ Utils/RXXPacker/RXXPackerConsole.pas
@@ -8,6 +8,16 @@ uses
 
 type
   TKMRXXPackerConsole = class
+  private class var
+    fParams: TStringList;
+    fSpritesSourcePath: string;
+    fRXXSavePath: string;
+    fPackToRXA: Boolean;
+    fRxs: TRXTypeSet;
+  private
+    class procedure OutputInstructions;
+    class procedure ParseArguments;
+    class procedure Pack;
   public
     class procedure Execute;
   end;
@@ -18,121 +28,118 @@ uses
   KM_Defaults, RXXPackerProc;
 
 const
-  RXX_TO_PACK: array [0..5] of TRXType = (
-    rxTrees,
-    rxHouses,
-    rxUnits,
-    rxGui,
-    rxGuiMain,
-    rxTiles);
+  // Everything except rxCustom
+  RXX_TO_PACK: TRXTypeSet = [rxTrees, rxHouses, rxUnits, rxGui, rxGuiMain, rxTiles];
+
 
 { TKMRXXPackerConsole }
-class procedure TKMRXXPackerConsole.Execute;
+class procedure TKMRXXPackerConsole.OutputInstructions;
+begin
+  Writeln('Arguments:');
+  Writeln(' - sld spritesloaddir %s - source folder');
+  Writeln(' - ssd spritessavedir %s - target folder');
+  Writeln(' - rxa packtorxa - pack RXA as well');
+  Writeln(' - all - pack all RX libraries');
+  Writeln(' - %s - pack specific RX library');
+  Writeln('');
+  Writeln('Usage examples:');
+  Writeln(' - RxxPacker.exe gui guimain houses trees units tileset');
+  Writeln(' - RxxPacker.exe all');
+  Writeln(' - RxxPacker.exe spritesBaseDir "C:\kmr_sprites\" units');
+end;
+
+
+class procedure TKMRXXPackerConsole.ParseArguments;
 var
-  fParams: TStringList;
-  I, K, cnt: Integer;
-  paramString: string;
+  I: Integer;
   rxType: TRXType;
-  lRxxPacker: TKMRXXPacker;
-  palettes: TKMResPalettes;
+begin
+  // Take in all params for simpler parsing down below
+  for I := 1 to ParamCount do
+  begin
+    Writeln('Arguments: ' + ParamStr(I));
+    fParams.Append(ParamStr(I));
+  end;
+
+  for I := 0 to fParams.Count - 1 do
+  begin
+    if (LowerCase(fParams[I]) = 'sld') or (LowerCase(fParams[I]) = 'spritesloaddir') then
+      if I < fParams.Count - 1 then
+      begin
+        fSpritesSourcePath := fParams[I+1];
+        fParams[I+1] := ''; // Make sure we dont parse it as some other key
+      end else
+        raise Exception.Create('spritesLoadDir directory not specified');
+
+    if (LowerCase(fParams[I]) = 'ssd') or (LowerCase(fParams[I]) = 'spritessavedir') then
+      if I < fParams.Count - 1 then
+      begin
+        fRXXSavePath := fParams[I+1];
+        fParams[I+1] := ''; // Make sure we dont parse it as some other key
+      end else
+        raise Exception.Create('spritesSaveDir directory not specified');
+
+    if (LowerCase(fParams[I]) = 'packtorxa') or (LowerCase(fParams[I]) = 'rxa') then
+      fPackToRXA := True;
+
+    for rxType := Low(TRXType) to High(TRXType) do
+    if LowerCase(fParams[I]) = 'all' then
+      fRxs := RXX_TO_PACK
+    else
+    if LowerCase(fParams[I]) = LowerCase(RX_INFO[rxType].FileName) then
+      fRxs := fRxs + [rxType];
+  end;
+end;
+
+
+class procedure TKMRXXPackerConsole.Pack;
+var
+  rxType: TRXType;
+  rxxPacker: TKMRXXPacker;
+  resPalettes: TKMResPalettes;
   tick: Cardinal;
 begin
-  if ParamCount = 0 then
-  begin
-    Writeln('No rx packages were set');
-    Writeln('');
-    Writeln('Arguments:');
-    Writeln(' - sld spritesloaddir %s - source folder');
-    Writeln(' - ssd spritessavedir %s - target folder');
-    Writeln(' - rxa packtorxa - pack RXA as well');
-    Writeln(' - all - pack all RX libraries');
-    Writeln(' - %s - pack specific RX library');
-    Writeln('');
-    Writeln('Usage examples:');
-    Writeln(' - RxxPacker.exe gui guimain houses trees units tileset');
-    Writeln(' - RxxPacker.exe all');
-    Writeln(' - RxxPacker.exe spritesBaseDir "C:\kmr_sprites\" units');
-    Exit;
-  end else
-  if ParamCount >= 1 then
+  ExeDir := ExpandFileName(ExtractFilePath(ParamStr(0)) + '..\..\');
+
+  rxxPacker := TKMRXXPacker.Create(ExeDir);
+  rxxPacker.SpritesSourcePath := fSpritesSourcePath;
+  rxxPacker.RXXSavePath := fRXXSavePath;
+  rxxPacker.PackToRXA := fPackToRXA;
+
+  resPalettes := TKMResPalettes.Create;
+  resPalettes.LoadPalettes(ExeDir + 'data\gfx\');
   try
-    Writeln('KaM Remake RXX Packer');
-
-    // Take in all params
-    fParams := TStringList.Create;
-    for I := 1 to ParamCount do
-      fParams.Append(ParamStr(I));
-
-    for I := 0 to fParams.Count - 1 do
-      Writeln('Arguments: ' + fParams[I]);
-
-    ExeDir := ExpandFileName(ExtractFilePath(ParamStr(0)) + '..\..\');
-    lRxxPacker := TKMRXXPacker.Create(ExeDir);
-    palettes := TKMResPalettes.Create;
-    palettes.LoadPalettes(ExeDir + 'data\gfx\');
-    try
-      I := 0;
-      cnt := ParamCount;
-      while I < cnt do // Skip 0, as this is the EXE-path
-      begin
-        Inc(I);
-        paramString := ParamStr(I);
-        if (LowerCase(paramString) = 'sld') or (LowerCase(paramString) = 'spritesloaddir') then
-        begin
-          if (I >= ParamCount) then
-          begin
-            Writeln('spritesLoadDir directory not specified');
-            Exit;
-          end;
-
-          Inc(I);
-
-          if not DirectoryExists(ParamStr(I)) then
-          begin
-            Writeln('spritesLoadDir directory does not exist: ' + ParamStr(I));
-            Exit;
-          end;
-          lRxxPacker.SpritesSourcePath := ParamStr(I);
-          Continue;
-        end;
-
-        if ((LowerCase(paramString) = 'ssd') or (LowerCase(paramString) = 'spritessavedir')) then
-        begin
-          if (I >= ParamCount) then
-          begin
-            Writeln('spritesSaveDir directory not specified');
-            Exit;
-          end;
-
-          Inc(I);
-          lRxxPacker.RXXSavePath := ParamStr(I);
-          Continue;
-        end;
-
-        if ((LowerCase(paramString) = 'packtorxa') or (LowerCase(paramString) = 'rxa')) then
-        begin
-          lRxxPacker.PackToRXA := True;
-
-          Continue;
-        end;
-
-        paramString := ParamStr(I);
-
-        for K := Low(RXX_TO_PACK) to High(RXX_TO_PACK) do
-        if (LowerCase(paramString) = 'all')
-        or (LowerCase(paramString) = LowerCase(RX_INFO[rxType].FileName)) then
-        begin
-          tick := GetTickCount;
-          lRxxPacker.Pack(RXX_TO_PACK[K], palettes, procedure (aMsg: string) begin Writeln(aMsg); end);
-          Writeln(RX_INFO[RXX_TO_PACK[K]].FileName + '.rxx packed in ' + IntToStr(GetTickCount - tick) + ' ms');
-        end;
-
-        Writeln('Unknown argument ' + paramString);
-      end;
-    finally
-      lRxxPacker.Free;
-      palettes.Free;
+    for rxType := Low(TRXType) to High(TRXType) do
+    if rxType in fRxs then
+    begin
+      tick := GetTickCount;
+      rxxPacker.Pack(rxType, resPalettes, procedure (aMsg: string) begin Writeln(aMsg); end);
+      Writeln(RX_INFO[rxType].FileName + '.rxx packed in ' + IntToStr(GetTickCount - tick) + ' ms');
     end;
+  finally
+    rxxPacker.Free;
+    resPalettes.Free;
+  end;
+end;
+
+
+class procedure TKMRXXPackerConsole.Execute;
+begin
+  Writeln('KaM Remake RXX Packer');
+  Writeln('');
+
+  fParams := TStringList.Create;
+  try
+    ParseArguments;
+
+    if fParams.Count = 0 then
+    begin
+      Writeln('No rx packages were set');
+      OutputInstructions;
+      Exit;
+    end;
+
+    Pack;
   except
     on E: Exception do
     begin
@@ -141,5 +148,6 @@ begin
     end;
   end;
 end;
+
 
 end.
