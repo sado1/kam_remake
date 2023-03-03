a433babbc0d32c26addd70b8b766c4fa4cc13f4b
Rey <levchenko.and@gmail.com>
2023-03-03 15:23:38 +0200

    Update pull_repos.bat

--- bat/pull_repos.bat
+++ bat/pull_repos.bat
@@ -6,13 +6,26 @@ call rsvars.bat
 
 @SET curr_directory=%cd%
 
+cd /D ..
+
+@REM update all submodules
+git submodule update --recursive --remote
+
+@REM pull TranslationManager
+cd /D "./Utils/TranslationManager (from kp-wiki)"
+
+git pull
+
+@REM pull private repo files
 cd /D %KMRPrivateRepoDir%
 
 git pull
 
+@REM pull resource repo
 cd /D %KMRResourcesRepoDir%
 
 git pull
 
+@REM get back to our directory
 cd /D %curr_directory% 
 
