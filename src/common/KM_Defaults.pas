932ac975530807d6faf51f62edda2f3c908116ff
rey <levchenko.and@gmail.com>
2023-04-05 17:39:34 +0300

    Change ArmyPower rates: utCavalry to 6.2 utPeasant to 1.2

--- src/common/KM_Defaults.pas
+++ src/common/KM_Defaults.pas
@@ -1115,8 +1115,8 @@ const
     1, 2.4, 5.2,    // utMilitia, utAxeFighter, utSwordsman
     2.2, 4,         // utBowman, utArbaletman
     2, 4,           // utPikeman, utHallebardman
-    3.3, 6,         // utHorseScout, utCavalry
-    5.3, 1.5, 1.5,  // utBarbarian, utPeasant, utSlingshot
+    3.3, 6.2,       // utHorseScout, utCavalry
+    5.3, 1.2, 1.5,  // utBarbarian, utPeasant, utSlingshot
     5.3, 2.1        // utMetalBarbarian, utHorseman
   );
 
