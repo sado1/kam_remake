ab8b74b96bd0e046cb1cc5e3b59109be2b93a6b2
Rey <levchenko.and@gmail.com>
2023-08-13 01:27:42 +0300

    Add missing {$IFNDEF NO_OGL} directive for MapUtil compilation

--- src/res/KM_ResSprites.pas
+++ src/res/KM_ResSprites.pas
@@ -47,8 +47,10 @@ type
     fPad: Byte; // Padding between sprites to avoid neighbour edge visibility
 
     procedure SetGFXData(aTexID: Cardinal; aSpriteInfo: TKMBinItem; aAtlasType: TKMSpriteAtlasType);
+    {$IFNDEF NO_OGL}
     procedure PrepareAtlases(aSpriteInfo: TBinArray; aMode: TKMSpriteAtlasType; aTexType: TKMTexFormat; var aBaseRAM, aColorRAM, aTexCount: Cardinal;
                              aFillGFXData: Boolean = True; aOnCheckTerminated: TBooleanFuncSimple = nil);
+    {$ENDIF}
   protected
     fRT: TRXType;
     fRXData: TRXData;
