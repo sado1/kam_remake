a2c1e2031de086061ba0ea33a6ba05394596d033
rey <levchenko.and@gmail.com>
2024-11-26 09:14:53 +0000

    Add OpenAL32.dll, which we took from openal-soft-1.24.0-bin.zip
    This way we don't require to Install OpenAL32 anymore

--- bat/copy_post_pack.bat
+++ bat/copy_post_pack.bat
@@ -75,6 +75,7 @@ xcopy ..\bass.dll "%kam_folder%"\bass.dll* /y /r /i
 xcopy ..\ogg.dll "%kam_folder%"\ogg.dll* /y /r /i
 xcopy ..\vorbis.dll "%kam_folder%"\vorbis.dll* /y /r /i
 xcopy ..\vorbisfile.dll "%kam_folder%"\vorbisfile.dll* /y /r /i
+xcopy ..\OpenAL32.dll "%kam_folder%"\OpenAL32.dll* /y /r /i
 xcopy ..\Installer\uninst_clean.bat "%kam_folder%"\uninst_clean.bat* /y /r /i
 xcopy ..\Installer\oalinst.exe "%kam_folder%"\oalinst.exe* /y /r /i
 
