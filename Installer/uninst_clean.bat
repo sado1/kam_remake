1787c390a8c02457d447aff6bb6780ff84551228
Rey <levchenko.and@gmail.com>
2025-03-16 21:14:23 +0000

    Installer - delete all temp count.dat when uninstall the game

--- Installer/uninst_clean.bat
+++ Installer/uninst_clean.bat
@@ -2,6 +2,16 @@ rmdir /S /Q logs
 
 erase /F /Q /S kmr_dev.xml
 
+@echo off
+setlocal
+
+REM Delete all count.dat files from sfx folders recursively, they are temp game files 
+for /r "data\sfx\" %%f in ("count.dat") do (
+    del "%%f"
+)
+
+endlocal
+
 erase /F /Q /S *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi *.stat
 
 erase /F /Q /S /A:H *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi *.stat
