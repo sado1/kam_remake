517e349c9344cad5227d12350af5d7a5e1c02312
sado <sado>
2025-03-23 15:49:05 +0100

    HTML docs generation - move document title from console arguments to a textfile, so UTF8 chars are preserved Fixes https://trello.com/c/EXIpqc3N/2297-fix-generation-of-ru-readme-title

--- bat/generate_docs.bat
+++ bat/generate_docs.bat
@@ -7,10 +7,10 @@ call rsvars.bat
 @SET curr_directory=%cd%
 cd ..\Docs\Readme
 
-%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_eng.md -o Readme_eng.html --metadata title="Getting started with Knights and Merchants: Remake"
-%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_ger.md -o Readme_ger.html --metadata title="Erste Schritte mit Knights and Merchants: Remake"
-%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_pol.md -o Readme_pol.html --metadata title="Pierwsze kroki z Knights and Merchants: Remake"
-%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_rus.md -o Readme_rus.html --metadata title="Начало работы с Knights and Merchants: Remake"
+%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_eng.md -o Readme_eng.html --metadata-file=".\Readme\metadata_eng.yml"
+%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_ger.md -o Readme_ger.html --metadata-file=".\Readme\metadata_ger.yml"
+%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_pol.md -o Readme_pol.html --metadata-file=".\Readme\metadata_pol.yml"
+%PandocDir_LOCAL%\pandoc.exe -s -f markdown getting-started_rus.md -o Readme_rus.html --metadata-file=".\Readme\metadata_rus.yml"
 
 cd /D %curr_directory%
 
