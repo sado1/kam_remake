4f852025b85046d2e4f673903d350860e7c02ff6
kromster80 <kromster80@gmail.com>
2025-07-31 16:10:02 +0200

    Removed unused const

--- src/common/KM_Defaults.pas
+++ src/common/KM_Defaults.pas
@@ -298,7 +298,6 @@ var
   WriteResourceInfoToTXT  :Boolean = False; //Whenever to write txt files with defines data properties on loading
   EXPORT_SPRITE_ATLASES   :Boolean = False; //Whenever to write all generated textures to PNG on loading (extremely time consuming)
   EXPORT_SPRITE_ATLASES_RXA:Boolean = False; //Whenever to write all loaded from .RXA files textures to PNG on loading (super extremely time consuming)
-  EXPORT_INFLUENCE        :Boolean = False;
   LOG_FONTS_RAM_USAGE     :Boolean = False;
   DBG_EXPORT_SOUNDS_DAT   :Boolean = False;
 
